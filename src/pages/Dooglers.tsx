a83d24245662b894beb68cb8ebf9b8f62e6d8db9
Piotr Ostrowski <piotrostr@google.com>
2022-09-20 14:49:27 +0100

    feat: show a doggo image from gs

--- src/pages/Dooglers.tsx
+++ src/pages/Dooglers.tsx
@@ -1,39 +1,18 @@
-// import { useState } from 'react';
 import { Link } from "react-router-dom";
-import { useStorage, useStorageDownloadURL, /* useStorageTask */ } from "reactfire";
-import { ref, /* UploadTaskSnapshot */ } from "firebase/storage";
+import { useStorage, useStorageDownloadURL } from "reactfire";
+import { ref } from "firebase/storage";
 
 export interface Doogler {
   name: string;
-  [key: string]: any; // TODO add rest
+  [asdf: string]: any; // TODO(piotrostr) add rest
 }
-
-// TODO add upload
-// export const UploadProgress = ({ uploadTask, storageRef }: any) => {
-//   const { status, data: uploadProgress } = useStorageTask<UploadTaskSnapshot>(uploadTask, storageRef);
-// 
-//   let percentComplete;
-// 
-//   if (status === 'loading') {
-//     percentComplete = '0%';
-//   } else {
-//     const { bytesTransferred, totalBytes } = uploadProgress;
-//     percentComplete = Math.round(100 * (bytesTransferred / totalBytes)) + '%';
-//   }
-// 
-//   return <span>{percentComplete} uploaded</span>;
-// }
-// 
-// export const UploadForm = () => {
-//   const storage = useStorage()
-//   const storageRef = ref(storage, 'dooglers');
-//   const [uploadTask, setUploadTask] = useState(null);
-// }
 
 export const Doogler = () => {
   const storage = useStorage();
-  const dooglerRef = ref(storage, 'dogs/stitch.jpg');
-
+  const dooglerRef = ref(
+    storage,
+    'craiyon_153130_google_dog.png',
+  );
   const { status, data: imageURL } = useStorageDownloadURL(dooglerRef);
 
   if (status === 'loading') {
@@ -45,14 +24,18 @@ export const Doogler = () => {
     );
   }
 
-  return <img src={imageURL} alt="dogs-stitch" />;
+  return (
+    <img src={imageURL} alt="dogs-stitch" style={{ maxWidth: 250 }} />
+  );
 }
 
 export const Dooglers = () => {
   return (
     <>
       <Link to="/">Go back</Link>
-      <Doogler />
+      <pre>
+        <Doogler />
+      </pre>
     </>
   )
 }
