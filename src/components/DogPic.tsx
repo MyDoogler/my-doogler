90755f1410a9e3bd05b94a6a424e35e4e167eba2
piotrostr <piotr.ostrowski@aol.com>
2022-09-22 17:02:22 +0100

    feat: add dog pics for signed up fellows instead of signin menu

---
+++ src/components/DogPic.tsx
@@ -0,0 +1,8 @@
+interface Props {
+  src: string;
+}
+export const DogPic = ({ src }: Props) => {
+  return (
+    <img src={src} alt="dog-pic" width={300} />
+  )
+}
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,6 +2,11 @@ import { EmailAuthProvider, GoogleAuthProvider } from "firebase/auth";
 import { StyledFirebaseAuth } from "react-firebaseui";
 import { useAuth, useSigninCheck } from "reactfire";
 import { Spinner } from "./Spinner";
+import { DogPic } from "./DogPic";
+
+import dog1 from "../assets/dogpic1.png"
+import dog2 from "../assets/dogpic2.png"
+import dog3 from "../assets/dogpic3.png"
 
 const uiConfig = {
   signInFlow: "popup",
@@ -11,6 +16,16 @@ const uiConfig = {
   ],
 };
 
+function DogPics() {
+  return (
+    <div className="dogpics">
+      <DogPic src={dog1} />
+      <DogPic src={dog2} />
+      <DogPic src={dog3} />
+    </div>
+  )
+}
+
 export function Login() {
   const auth = useAuth();
   const { status, data: user } = useSigninCheck();
@@ -18,7 +33,7 @@ export function Login() {
     return <Spinner />
   }
   if (user.signedIn) {
-    return
+    return <DogPics />
   }
   return <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
 }
