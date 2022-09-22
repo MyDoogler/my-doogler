96fe35cbfb18a9497e5584eb40a640aea24ba309
piotrostr <piotr.ostrowski@aol.com>
2022-09-22 13:37:11 +0100

    show signed in user

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
+import { useSigninCheck, useUser } from "reactfire";
 import paw from "../assets/paw.svg";
 
 export const Header = () => {
+  const { status, data: signInCheckResult } = useSigninCheck()
+  const { status: userStatus, data: user } = useUser()
   return (
     <header
       style={{
@@ -16,16 +19,29 @@ export const Header = () => {
       </div>
       <div
         style={{
-          flex: "2.8",
+          flex: "2",
           display: "flex",
-          justifyContent: "space-between",
+          justifyContent: "space-around",
         }}
       >
         <Link to="/">Home</Link>
         <Link to="/dooglers">Dooglers</Link>
         <Link to="/create">Create</Link>
       </div>
-      <div style={{ flex: "1" }} />
+      <div style={{ flex: "1", display: 'flex', justifyContent: 'center' }}>
+        {
+          status === 'success' && signInCheckResult.signedIn ? (
+            userStatus === 'success' && user && (
+              <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                <img src={user.photoURL || ""} alt="user-profile" width={30} height={30} style={{ borderRadius: 999, marginRight: 10 }} />
+                <div>{user.email?.split('@')[0]}</div>
+              </div>
+            )
+          ) : (
+            <Link to="/">Sign In</Link>
+          )
+        }
+      </div>
     </header>
   );
 };
